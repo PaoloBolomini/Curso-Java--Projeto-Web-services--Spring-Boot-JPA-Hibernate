8a341e20d9b451d536a8180dfd5085f6a2e2c0d4
Paolo Bolomini <paolo.bolomini@gmail.com>
2024-06-24 21:19:02 -0300

    Criando tabela Order ẽ comentários JPA parte 1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,14 @@ public class User implements Serializable {
 	private String password;
 	private String phone;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+
 	public User() {
 		
 	}
@@ -33,6 +44,10 @@ public class User implements Serializable {
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public long getId() {
